632e32d60e0368508e684e0fc9fbfce0e4b22522
Ankita Prakash <ankita.prakash1998@gmail.com>
2020-04-19 03:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import dash_html_components as html
 import folium
 
 app = dash.Dash(__name__)
-server = app.server()
 
 app.layout = html.Div([
     html.H1("Map"),
@@ -13,4 +12,4 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run()
