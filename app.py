395039428f339b1622a4bbe84af4bfaf563e76d1
Ankita Prakash <ankita.prakash1998@gmail.com>
2020-04-19 03:28:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+# Import required libraries
+import os
+from random import randint
+
 import folium
 
-app = dash.Dash(__name__)
+import flask
+import dash
+from dash.dependencies import Input, Output, State, Event
+import dash_core_components as dcc
+import dash_html_components as html
+
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
 
 app.layout = html.Div([
     html.H1("Map"),
@@ -11,5 +20,7 @@ app.layout = html.Div([
 
 ])
 
+
+# Run the Dash app
 if __name__ == '__main__':
-    app.run()
+    app.server.run(debug=True, threaded=True)
